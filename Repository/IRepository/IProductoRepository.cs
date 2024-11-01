68309eb6a3fe4aab8d8ba55a1e1661c641891c00
Lancelot <lancelot@sunset.es>
2024-11-01 13:34:23 -0500

    Gestion inventario

---
+++ Repository/IRepository/IProductoRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventario.Repository.IRepository
+{
+    public interface IProductoRepository
+    {
+    }
+}
